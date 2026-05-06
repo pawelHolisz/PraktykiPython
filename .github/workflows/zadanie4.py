a002d3da514d85d2fdcff8b4de919c92c83729cd
pawelHolisz <RexoNight997@gmail.com>
2026-05-06 11:50:42 +0200

    Delete .github/workflows/zadanie4.py

--- .github/workflows/zadanie4.py
+++
@@ -1,7 +0,0 @@
-lista =[2,0,5,1,6,3]
-
-for j in range(len(lista)):
-    for i in range(len(lista) - 1):
-        if(lista[i] > lista[i+1]):
-                lista[i], lista[i+1] = lista[i+1], lista[i];
-                print(lista);
